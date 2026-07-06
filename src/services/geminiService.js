44fb7f79d7f4e52a550ba0e579edc57ef67c73ff
JohnTech-Loma <jhondejesuscastrovalle@gmail.com>
2026-07-06 15:40:29 -0500

    mejorando modelo a gemini-3

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -7,7 +7,7 @@ const geminiService = async (message) => {
 
 	try {
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${GEMINI_API_KEY}`,
       {
         contents: [
           {
